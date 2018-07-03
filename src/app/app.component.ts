e9ab3cf599ce4f19ceafe7a0b8a02efc15f061fc
yarivg <yarivgavriel2@gmail.com>
2018-07-03 09:58:56 +0300

    create capture image and reset camera function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,44 +13,85 @@ import { ISimpleResponse } from './shared/interfaces/simple.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './bootstrap.min.css']
 })
 export class AppComponent implements OnInit {
   location = {};
   observable$: Observable<ISimpleResponse>;
+  isImageCaptured: boolean;
+  canvas: HTMLCanvasElement;
 
-  constructor(private http: HttpClient, private store: Store<IAppState>) {}
+  constructor(private http: HttpClient, private store: Store<IAppState>) { }
 
-  setPosition(position){
+  setPosition(position) {
     // this.location = position.coords;
     console.log(position.coords);
   }
 
   ngOnInit() {
 
-    if(navigator.geolocation){
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.setPosition);
     };
 
-    this.observable$ = this.http.get<ISimpleResponse>('/api/public/simple');
+    // this.observable$ = this.http.get<ISimpleResponse>('/api/public/simple');
 
-    this.store.dispatch({
-      type: USER_GET
-    });
+    // this.store.dispatch({
+    //   type: USER_GET
+    // });
+
+    this.isImageCaptured = false
+    this.setCanvas()
   }
 
   // camera
-  @ViewChild('video') video:any; 
-// note that "#video" is the name of the template variable in the video element
+  @ViewChild('video') video: any;
+  // note that "#video" is the name of the template variable in the video element
 
-ngAfterViewInit() {
-  let _video=this.video.nativeElement;
-  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-                          .then(stream => {
-                            _video.src = window.URL.createObjectURL(stream);
-                            _video.play();
-                          })
+  ngAfterViewInit() {
+    let _video = this.video.nativeElement;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+          _video.src = window.URL.createObjectURL(stream);
+          _video.play();
+        })
+    }
   }
-}
+
+  setCanvas() {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width  = 100;
+    this.canvas.height = 100;
+    this.canvas.style.position = "absolute";
+    
+    this.canvas.style.top = "25%";
+    this.canvas.style.left = "25%";
+    this.canvas.style.width = "50%";
+    this.canvas.style.height = "50%";
+
+  }
+
+  generateThumbnail() {
+    this.isImageCaptured = true;
+    
+    var videoElement = <HTMLCanvasElement> document.getElementById("video");
+    videoElement = document.getElementById("video") as HTMLCanvasElement;
+
+    var ctx = this.canvas.getContext('2d');
+    ctx.drawImage(videoElement, 0, 0, 100, 100);
+    
+    document.body.appendChild(this.canvas);
+  }
+
+  upload() {
+    window.alert('uploading image. thank you!')
+    console.log('uploaded')
+  }
+
+  resetCamera() {
+    this.isImageCaptured = false;
+    console.log('resetting camera');
+    document.body.removeChild(this.canvas);
+  }
 }
