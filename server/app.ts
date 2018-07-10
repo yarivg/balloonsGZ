98ec9b9ac3c3e551ef7d64252465e6704ce5b4db
yarivg <yarivgavriel2@gmail.com>
2018-07-10 10:07:00 +0300

    new request example

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { json, urlencoded } from "body-parser";
 import * as compression from "compression";
 import * as express from "express";
 import * as path from "path";
+import * as http from "http";
 
 import { feedRouter } from "./routes/feed";
 import { loginRouter } from "./routes/login";
@@ -9,6 +10,11 @@ import { protectedRouter } from "./routes/protected";
 import { publicRouter } from "./routes/public";
 import { userRouter } from "./routes/user";
 
+var util = require('util')
+var https = require('https');
+
+const loginToken = "otdSlvsbPJvFfvQPWywbMDDm2Pyll8j4ZcDhNgqp"
+const reporterID = 56
 const app: express.Application = express();
 
 app.disable("x-powered-by");
@@ -24,11 +30,47 @@ app.use("/api/public", publicRouter);
 app.use("/api/feed", feedRouter);
 app.use("/api/user", userRouter);
 
+var options = {
+    host: 'www.res-cue.com',
+    port: 443,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      "Access-Control-Allow-Origin": '*'
+    }
+}
+
+// New report
+app.post('/report', (req, res) => {
+  let path = '/app/ws/report/new/report'
+  let reqOptions = JSON.parse(JSON.stringify(options))
+  reqOptions['path'] = path
+
+  let sayVURequest = https.request(options, (sayVURes) =>
+  {
+      sayVURes.setEncoding('utf8');
+      sayVURes.on('data', function (chunk) {
+        console.log("body: " + chunk);
+        res.send(chunk)
+        res.end()
+      })
+  })
+
+  util.format('EmergencySubCatID=0&LocationX={0}&loginToken={1}&AddedTimeMilliseconds={2}&ReporterID={3}&LocationY={4}&EmergencyCatID=6',
+              req.body.location.latitude.toString(),
+              loginToken, 
+              reporterID, 
+              Date.now().toString(), 
+              req.body.location.longtitude.toString())
+  sayVURequest.write('EmergencySubCatID=0&LocationX=31.508143&loginToken=otdSlvsbPJvFfvQPWywbMDDm2Pyll8j4ZcDhNgqp&AddedTimeMilliseconds=1531137892292&ReporterID=56&LocationY=34.592048&EmergencyCatID=6');
+  sayVURequest.end();
+})
+
 // if (app.get("env") === "production") {
   // in production mode run application from dist folder
 
   // PRODUCTION MODE
-  app.use(express.static(path.join(__dirname, "/../client")));
+app.use(express.static(path.join(__dirname, "/../client")));
 // }
 
 // catch 404 and forward to error handler
