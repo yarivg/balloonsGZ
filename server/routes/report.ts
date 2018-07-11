0cf67432a66cc32d6c3a6e449833766584e3ffb1
yarivg <yarivgavriel2@gmail.com>
2018-07-11 14:35:53 +0300

    add comment

--- server/routes/report.ts
+++ server/routes/report.ts
@@ -23,7 +23,7 @@ reportRouter.post("/", (req: Request, res: Response) => {
             lat: req.body.lat,
             image: req.body.imageBase64,
             heading: req.body.azimuth,
-            tag: req.body.tag,
+            tag: req.body.tag,// category - string num
             description: req.body.description,
             pitch: 0,
             token: seeVUToken
