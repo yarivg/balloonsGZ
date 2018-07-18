a6ffbb3e6291bc9245a9f6aa0c43e06ad0131ee1
Noam <noamdob@gmail.com>
2018-07-18 18:37:37 +0300

    added service to communicate with sayvu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  serverBaseURL: 'http://localhost:4300',
+  serverLayersURL: 'http://dev.res-cue.com:8082',
+  token: 'kljgfzedtapamqpcbwksviwxeofzfeqqrpvphtbwhyocnstakgoduzprhybrxxjwsrctloghthhshqdgaaecqvofmuidjfplosyyokfukkrpjclayvrnmbrnojohyjjm',
+  callcenter: '1'
 };
