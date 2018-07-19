91decee3ea1a9fd3d9759a14d728f3b7306af847
Noam <noamdob@gmail.com>
2018-07-20 00:10:58 +0300

    testing something

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serverBaseURL: 'http://localhost:4300',
-  serverLayersURL: 'https://dev.res-cue.com:8082',
+  serverLayersURL: 'https://callcenter.res-cue.com:8082',
   token: 'kljgfzedtapamqpcbwksviwxeofzfeqqrpvphtbwhyocnstakgoduzprhybrxxjwsrctloghthhshqdgaaecqvofmuidjfplosyyokfukkrpjclayvrnmbrnojohyjjm',
   callcenter: '1'
 };
