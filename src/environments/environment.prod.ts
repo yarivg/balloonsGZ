f1dbe10e447f8f497bc2f1e3a92e143a6ebb901f
Noam <noamdob@gmail.com>
2018-07-21 13:03:28 +0300

    changed cred for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serverBaseURL: 'http://localhost:4300',
-  serverLayersURL: 'https://callcenter.res-cue.com:8082',
+  serverLayersURL: 'https://res-cue.com',
   token: 'leeroezpsnyoecdjvqofomqpwjvrjcybdvcpewkwhjbvkwdeqewlyfhtyprhxngbmhrdxzjupigeounbiwzgdbzuuydtykguzkxoghqjnjisazxwaswjwscpuyogdzgr',
   callcenter: '65'
 };
