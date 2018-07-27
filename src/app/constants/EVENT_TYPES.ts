92f8a69c84e83a9c667bd8350793d4b049a529f1
yoni2356 <yoni2356@gmail.com>
2018-07-27 06:08:54 +0300

    creating new constants

---
+++ src/app/constants/EVENT_TYPES.ts
@@ -0,0 +1,5 @@
+export const EVENT_TYPES = {
+  FIRE: 'fire',
+  BALLOON: 'balloon',
+  KITE: 'kite'
+};
---
+++ src/app/constants/FIRE_SIZES.ts
@@ -0,0 +1,5 @@
+export const FIRE_SIZES = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large'
+};
