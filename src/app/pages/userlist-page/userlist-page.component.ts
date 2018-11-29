af493bebbf4da73df99510a2bde4bcdb1eadcad1
Jiang Lingshuo <jianglingshuo0526@gmail.com>
2018-11-30 00:42:34 +0200

    initialize material design layout for HTML page

--- src/app/pages/userlist-page/userlist-page.component.ts
+++ src/app/pages/userlist-page/userlist-page.component.ts
@@ -7,6 +7,10 @@ import {FIRE_SIZES} from '../../constants/FIRE_SIZES';
 import {ReportService} from '../../../services/report.service';
 import {UserAgentService} from '../../../services/userAgent.service';
 import {BURNING_SIZES, CATEGORIES_NAMES, FLYING_OBJECTS_HEIGHT} from '../../constants/HebrewTranslations';
+import {ApiMethod} from 'ngx-facebook/dist/umd/providers/facebook';
+import {Http} from '@angular/http';
+import * as environment from '../../../../.configenv';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 declare var $: any;
 
@@ -16,8 +20,32 @@ declare var $: any;
   styleUrls: ['./userlist-page.component.scss']
 })
 
-export class UserlistPageComponent implements OnInit{
+export class UserlistPageComponent implements OnInit {
+  users;
+  constructor(private http: HttpClient) {}
   ngOnInit() {
-
+    this.getOnlineFacebookFriends();
   }
+
+  getOnlineFacebookFriends() {
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+    let reportURL = '';
+    if (process.env.NODE_ENV !== 'production') {
+      reportURL = environment.config.serverBaseURL[1];
+    } else {
+      reportURL = environment.config.serverBaseURL[0];
+    }
+    this.http.post(reportURL.toString() + `/api/user/online`, {}, options).subscribe(data => {
+      console.log(data);
+      this.users = data;
+      }, error => {
+      console.error(error);
+    });
+  }
+
 }
