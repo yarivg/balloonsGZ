0d619b863fc6cc9aa16f983995ce70f2db490a82
Jiang Lingshuo <jianglingshuo0526@gmail.com>
2018-11-29 16:25:42 +0200

    config webcam-page.component.ts

--- src/app/pages/webcam-page/webcam-page.component.ts
+++ src/app/pages/webcam-page/webcam-page.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {UserAgentService} from '../../../services/userAgent.service';
+import {Subject} from 'rxjs';
+import {Observable} from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
   selector: 'app-webcam-page',
@@ -8,6 +10,38 @@ import {UserAgentService} from '../../../services/userAgent.service';
 })
 
 export class WebcamPageComponent implements  OnInit {
+  // Toogle webcam on/off
+  public showWebCam = true;
+  public allowCameraSwitch = true;
+  public multipleWebcamsAvalable = false;
+  public deviceId: string;
+  public videoOptions: MediaTrackConstraints = {
+    // width: {ideal: 1024},
+    // height: {ideal:: 576}
+  };
+  public errors: WebcamInitError[] = [];
+  // Latest snapshot
+  public webcamImage: WebcamImage = null;
+  // Webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+  // switch to next / previous / specific webcam; true / false
+  private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
   ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcamsAvalable = mediaDevices && mediaDevices.length > 1;
+      });
+  }
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+  public toggleWebcamm(): void {
+    this.showWebCam = !this.showWebCam;
+  }
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+  public showNextWebcam(directionOrDeviceId: boolean|string): void {
+    this.nextWebcam.next(directionOrDeviceId);
   }
 }
